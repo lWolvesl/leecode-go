338b7910522474854fbf93b6739ee78f7142960d
example <example@example.com>
2023-04-09 22:30:08 +0800

    routine

--- 2023/04/2399.go
+++ 2023/04/2399.go
@@ -1,5 +1,38 @@
 package leecode
 
 func Q2399(s string, distance []int) bool {
+	type index struct {
+		start int
+		end   int
+	}
 
+	m := make(map[rune]index)
+
+	for i, v := range s {
+		if _, ok := m[v]; ok {
+			m[v] = index{m[v].start, i}
+			dis := m[v].end - 1 - m[v].start
+			if distance[int(v)-97] != dis {
+				return false
+			}
+		} else {
+			m[v] = index{i, -1}
+		}
+	}
+
+	return true
+}
+
+func Q2399_1(s string, distance []int) bool {
+	for index := range s {
+		site := s[index] - 'a'
+		if distance[site] == -1 {
+			continue
+		}
+		if index+distance[site]+1 >= len(s) || s[index] != s[index+distance[site]+1] {
+			return false
+		}
+		distance[site] = -1
+	}
+	return true
 }
--- main.go
+++ main.go
@@ -7,4 +7,8 @@ import (
 
 func main() {
 	log.Println(leecode.Q2427(25, 30))
+	s := "abs"
+	log.Println(int(s[0]))
+	a := s[1] - 'a'
+	log.Println(a)
 }
