deb835814777c0df4d216b0444a7e279d7e40fee
Wolves <yangwolves@foxmail.com>
2023-07-24 19:14:33 +0800

    routine

---
+++ 2023/07/771.go
@@ -0,0 +1,15 @@
+package lee771
+
+func numJewelsInStones(jewels string, stones string) int {
+	j := make(map[int32]int, len(jewels))
+	for _, v := range jewels {
+		j[v] = 1
+	}
+	count := 0
+	for _, v := range stones {
+		if j[v] == 1 {
+			count++
+		}
+	}
+	return count
+}
